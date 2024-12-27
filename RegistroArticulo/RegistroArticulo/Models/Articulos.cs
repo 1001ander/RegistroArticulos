349e98c8b52e5d1a3cb02d44523fe057edf77293
Anderson <@1001ander>
2024-12-26 20:32:47 -0400

    Se creo la carpeta Models y el modelo  Articulos.

---
+++ RegistroArticulo/RegistroArticulo/Models/Articulos.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroArticulo.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Descripcion es Obligatorio")]
+        [StringLength(100, ErrorMessage = "La Descripcion no puede exceder los 100 caracteres")]
+        
+         public string? Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Campo Cantidad Es Obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "La Cantidad debe ser Mayor a 0")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "El Campo Precio es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El Precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+
+
+
+
+
+    }
+}
